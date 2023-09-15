adf1b26357b50262f88744259888b89901662546
caner <caner@CANER>
2023-09-15 15:17:58 -0500

    Add project files.

---
+++ AdvancedDatabaseandORMConcepts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedDatabaseandORMConcepts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
