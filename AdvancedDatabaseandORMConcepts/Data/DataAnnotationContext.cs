01648b230c9809926f93a334a609ad13c2fce19f
caner <caner@CANER>
2023-09-16 02:37:10 -0500

    lab4 added

---
+++ AdvancedDatabaseandORMConcepts/Data/DataAnnotationContext.cs
@@ -0,0 +1,14 @@
+﻿using AdvancedDatabaseandORMConcepts.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AdvancedDatabaseandORMConcepts.Data
+{
+    public class DataAnnotationContext : DbContext
+    {
+        public DataAnnotationContext(DbContextOptions<DataAnnotationContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; } = default!;
+        public DbSet<Room> Rooms { get; set; } = default!;
+    }
+}
